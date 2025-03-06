b4f228444c851c2796c909c4a4071f3344c1716c
rexona8 <rexona8@yandex.ru>
2025-03-06 21:18:01 +0300

    create new project bakers

---
+++ firstPoint/ProjectBakery.java
@@ -0,0 +1,38 @@
+package firstPoint;
+
+import java.util.Scanner;
+
+public class ProjectBakery {
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        String order;
+        while (true){
+            System.out.printf("Input your order: ");
+            order = iScanner.nextLine();
+            if (order.matches("[a-zA-Z ]+")){
+                break;
+            }
+            else {
+                System.out.println("If you want to place an order, enter the order name");
+            }
+        }
+    
+        System.out.printf("Input how many orders: ");
+        int howMany = 0;
+        while (!iScanner.hasNextInt()){
+            System.out.printf("Input how many orders: ");
+            iScanner.next();
+        }
+        howMany = iScanner.nextInt();
+        System.out.printf("Enter how many people: ");
+        boolean flag = iScanner.hasNextInt();
+        System.out.println(flag);
+        int howManyPeolple = iScanner.nextInt();
+        System.out.println(howManyPeolple);
+
+        System.out.printf("Hi that's your order: %d %s for %d persons please",
+        howMany, order, howManyPeolple);
+        iScanner.close();
+    }
+}
---
+++ firstPoint/SchemePoints.java
@@ -0,0 +1,22 @@
+package firstPoint;
+
+public class SchemePoints {
+    public static void main (String[] args) {
+        int [][]arr = new int[5][5];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (int) (Math.random() * 100);
+                if (arr[i][j] < 10){
+                    arr[i][j] = (int) (Math.random() * 100);
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
