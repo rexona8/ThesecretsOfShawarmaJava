7dae584850d241c948a87987421afe0acee4c1ac
rexona8 <rexona8@yandex.ru>
2025-03-08 23:21:39 +0300

    added new methods for idea

--- firstPoint/IdeaForWork.java
+++ firstPoint/IdeaForWork.java
@@ -1,19 +1,43 @@
 package firstPoint;
 
 public class IdeaForWork {
-    public static void main(String[] args) {
-        int value = 225;
+    public static void HowNumbers(int value) {
         int count = 0;
 
-        while (value != 0){
-            value /= 10;
-            count++;
-        }
+        if (value == 0) {
+            count = 1;
+        } else {
+            while (value != 0) {
+                value /= 10; // Уменьшаем число на один разряд
+                count++;
+            }
         System.out.println(count);
+        }
     }
-}
 
 // в цикле do while, сначала выполняется действие, затем идет проверка
 
 // цикл switch проверяет разные кейсы, case 1  и т.д. проверяет переменные и какое условие
 // при взаимодействии с ними, можно нескольким кейсам проверку на одно условие сделать, нужен break
+    
+    public static void FillArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 100);
+        }
+    }
+
+    public static void PrintArr(int[] array) {
+        for (int item : array) {
+            System.out.print(item + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+    int []massiv = new int[10];
+    
+    HowNumbers(225);
+    FillArr(massiv);
+    PrintArr(massiv);
+    }
+}
