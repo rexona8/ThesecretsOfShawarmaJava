03ba2f86e86c162d2327334ea610dd28aaf5164f
rexona8 <rexona8@yandex.ru>
2025-03-10 15:29:56 +0300

    added new the maximum number of consecutive numbers

--- firstPoint/SchemePoints.java
+++ firstPoint/SchemePoints.java
@@ -2,21 +2,44 @@ package firstPoint;
 
 public class SchemePoints {
     public static void main (String[] args) {
-        int [][]arr = new int[5][5];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = (int) (Math.random() * 100);
-                if (arr[i][j] < 10){
-                    arr[i][j] = (int) (Math.random() * 100);
-                }
-            }
-        }
+        // int [][]arr = new int[5][5];
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr[i].length; j++) {
+        //         arr[i][j] = (int) (Math.random() * 100);
+        //         if (arr[i][j] < 10){
+        //             arr[i][j] = (int) (Math.random() * 100);
+        //         }
+        //     }
+        // }
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.printf("%d ", arr[i][j]);
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr[i].length; j++) {
+        //         System.out.printf("%d ", arr[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+
+        int []arr = new int[]{1,2,0,1,1,1,0,2,2,2,2};
+        int count = 1;
+        int maxCount = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i-1])
+            {
+                // Если текущий элемент равен предыдущему, увеличиваем длину последовательности
+                count++;
             }
-            System.out.println();
+            else {
+            // Если последовательность прерывается, обновляем максимальную длину
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            // Сбрасываем текущую длину последовательности
+            count = 1;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+            }
         }
+        System.out.printf("Максимальное число подряд идущих чисел будет равно %d", maxCount);
     }
 }
