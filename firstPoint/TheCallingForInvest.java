d33afe4355267397508db6b0e6c2f064c68b913f
rexona8 <rexona8@yandex.ru>
2025-03-10 13:43:22 +0300

    added simple name request and start name request + date

---
+++ firstPoint/TheCallingForInvest.java
@@ -0,0 +1,18 @@
+package firstPoint;
+import java.util.Scanner;
+
+public class TheCallingForInvest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // System.out.print("Input your name: "); Просто печатаю имя в 2 вариациях
+        // String name = scanner.nextLine();
+        // System.out.print("Hi, " + name + "!");
+        // System.out.println();
+        // String name2 = new String("Hi, %s!");
+        // System.out.printf(name2, name);
+
+        System.out.print("Input your name: ");
+        String name = scanner.nextLine();
+    }
+}
