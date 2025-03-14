c60acf427ed75be6a4111af21d80119e6c2b3786
rexona8 <rexona8@yandex.ru>
2025-03-14 16:02:27 +0300

    added new temporary task in Arena

--- firstPoint/Arena.java
+++ firstPoint/Arena.java
@@ -1,19 +1,20 @@
 package FirstPoint;
 
-import java.util.Scanner;
+import java.util.Random;
+// import java.util.Scanner;
 
 public class Arena {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input number: ");
-        int number = scanner.nextInt();
-        scanner.close();
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Input number: ");
+        // int number = scanner.nextInt();
+        // scanner.close();
+        Random random = new Random();
+        int randNum = random.nextInt(100);
 
-        if (number < 100 || number > 999){
-            System.out.print("Incorrect number!");
-        }
-        else{
-            System.out.print(number % 10);
-        }
+        System.out.println(randNum);
+        System.out.printf("Случайное число: %d. Первая цифра (десятки): %d. Вторая цифра (единицы): %d. Наибольшая цифра: %d.", 
+                          randNum, randNum / 10, randNum % 10, Math.max(randNum / 10, randNum % 10));
+
     }
 }
