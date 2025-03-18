ee5500df83cf4818ce7ece26e9ec360a2066fd9c
rexona8 <rexona8@yandex.ru>
2025-03-18 19:46:10 +0300

    added new замес, предыдущая задача решается с помощью массива и со всеми проверками

--- firstPoint/Arena.java
+++ firstPoint/Arena.java
@@ -1,24 +1,62 @@
 package FirstPoint;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 // import java.util.Random;
 import java.util.Scanner;
+
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа. Не использовать строки для расчета.
+
+
+// интересно попробовать через массив или коллекцию
 
 public class Arena {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input number: ");
-        int number1 = scanner.nextInt();
-        int onetwonum = number1 % 100;
-        int middle = onetwonum / 10;
+        System.out.print("Input number: "); // просим число из консоли через scanner
+        int number = scanner.nextInt();
 
-        if (number1 == 0) {
-            System.out.println("Ошибка: деление на ноль!");
-        } else if (number1 > 999 || number1 < 100) {
-            System.out.println("Некорректное число, введите трехзначное число");
-        } else {
-            System.out.printf("Вторая цифра трехзначного числа: %d", middle);
+        if (!checkNum(number)){
+            System.out.println("Введите корректное число");
+            return;
         }
-        
+
+        int[] array = numArray(number);
+        System.out.println(array[1]);
         scanner.close();
+    }
+
+    public static int[] numArray(int number){ // метод перевода числа через массив с помощью коллекции
+        ArrayList<Integer> list = new ArrayList<>();
+        int val = number;
+
+        while (val > 0){
+            list.add(val % 10);
+            val /=10 ;
+        }
+
+        int size = list.size();
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++){
+            result[i] = list.get(size - 1 - i);
+        }
+
+        return result;
+    }
+
+    public static boolean checkNum (int number){ // проверка числа на ноль и на трехзначное
+        int value = number;
+
+        if (value == 0){
+            System.out.println("Ошибка деления на ноль");
+            return false;
+        }
+        if (value > 999 || value < 100){
+            System.out.println("Некорректное число, введите трехзначное число");
+            return false;
+        }
+
+        return true;
     }
 }
