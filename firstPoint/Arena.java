915d98ac23fa78047a5ab7530b7c9108f0db8f43
rexona8 <rexona8@yandex.ru>
2025-03-18 17:07:28 +0300

    added new decision new task the second digit of a three-digit number

--- firstPoint/Arena.java
+++ firstPoint/Arena.java
@@ -8,15 +8,17 @@ public class Arena {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input number: ");
         int number1 = scanner.nextInt();
+        int onetwonum = number1 % 100;
+        int middle = onetwonum / 10;
 
         if (number1 == 0) {
             System.out.println("Ошибка: деление на ноль!");
-        } else if (number1 % 7 == 0 && number1 % 23 == 0) {
-            System.out.println("Кратно 7 и 23");
+        } else if (number1 > 999 || number1 < 100) {
+            System.out.println("Некорректное число, введите трехзначное число");
         } else {
-            System.out.println("Не кратно 7 и 23");
+            System.out.printf("Вторая цифра трехзначного числа: %d", middle);
         }
-      
+        
         scanner.close();
     }
 }
