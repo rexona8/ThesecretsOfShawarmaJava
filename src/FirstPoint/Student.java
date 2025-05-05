2c32ed4ccb39ad6658f5de631dfa4888b90846f2
rexona8 <rexona8@yandex.ru>
2025-05-05 20:12:32 +0300

    new work with constructors on Student

--- src/FirstPoint/Student.java
+++ src/FirstPoint/Student.java
@@ -2,57 +2,52 @@ package FirstPoint;
 
 public class Student {
 
+    Student (long idcardNumber2, String name2,
+    String surname2, int year2, int mathScore2, int econScore2, int langScore2) {
+        this (idcardNumber2, name2, surname2, year2);
+        this.mathScore = mathScore2;
+        this.econScore = econScore2;
+        this.langScore = langScore2;
+    }
+
+    Student (long idcardNumber3, String name3, String surname3, int year3) {
+        this.idcardNumber = idcardNumber3;
+        this.name = name3;
+        this.surname = surname3;
+        this.year = year3;
+    }
+
+    Student () {
+
+    }
+
     long idcardNumber;
     String name;
     String surname;
     int year;
-    byte mathAvScore;
-    byte econAvScore;
-    byte langAvScore;
+    int mathScore;
+    int econScore;
+    int langScore;
+
+    public String toString() {
+        return "Студент" +
+               "\n Номер карты " + " " + idcardNumber +
+               "\n Имя " + " " + name +
+               "\n Фамилия " + " " + surname +
+               "\n Сколько лет " + " " + year +
+               "\n Балл по алгебре " + " " + mathScore +
+               "\n Балл по экономике " + " " + econScore +
+               "\n Балл по английскому языку " + " " + langScore + "\n";
+    }
 }
 
 class StudentTest {
-
-    double averageScore (Student st) {
-        double average = (double) (st.mathAvScore + st.econAvScore + st.langAvScore) / 3;
-        System.out.println("Номер в картотеке: " + st.idcardNumber);
-        System.out.println("Год обучения: " + st.year);
-        System.out.println("Средняя оценка студента " + st.name + " " + st.surname + ": " + average);
-        System.out.println();
-        return average;
-    }
-
     public static void main (String[] args) {
-        Student student1 = new Student();
-        student1.idcardNumber = 1033;
-        student1.name = "Nikita";
-        student1.surname = "Markov";
-        student1.year = 2022;
-        student1.mathAvScore = 4;
-        student1.econAvScore = 4;
-        student1.langAvScore = 5;
-    
-        Student student2 = new Student();
-        student2.idcardNumber = 1455;
-        student2.name = "Bogdan";
-        student2.surname = "Byrkov";
-        student2.year = 2023;
-        student2.mathAvScore = 3;
-        student2.econAvScore = 2;
-        student2.langAvScore = 2;
-    
+        Student student1 = new Student(456521, "Dik", "Mitchel", 43, 5, 3, 2);
+        System.out.println(student1);
+        Student student2 = new Student(65456354, "Zak", "Glover", 22);
+        System.out.println(student2);
         Student student3 = new Student();
-        student3.idcardNumber = 997;
-        student3.name = "John";
-        student3.surname = "Taylor";
-        student3.year = 2022;
-        student3.mathAvScore = 5;
-        student3.econAvScore = 5;
-        student3.langAvScore = 2;
-
-        StudentTest studentTest = new StudentTest();
-        studentTest.averageScore(student1);
-        studentTest.averageScore(student2);
-        studentTest.averageScore(student3);
+        System.out.println(student3);
     }
 }
