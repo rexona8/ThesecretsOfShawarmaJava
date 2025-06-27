e5ade6a1e6ea2c73ccf9147b106d0dcd90970431
rexona8 <rexona8@yandex.ru>
2025-06-27 12:04:01 +0300

    working out with switch-case in Arena.java

--- src/FirstPoint/Arena.java
+++ src/FirstPoint/Arena.java
@@ -1,7 +1,34 @@
 package FirstPoint;
 
+import java.util.Scanner;
+
 public class Arena {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Input number of month 2025: ");
+        int month = scanner.nextInt();
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+            System.out.println("31 days in a month");
+            break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+            System.out.println("30 days in a month");
+            break;
+            case 2:
+            System.out.println("28 days in a month");
+            break;
+            default:
+            System.out.println("There is no such month");
+        }
     }
 }
